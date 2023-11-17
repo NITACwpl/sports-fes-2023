a3ced1a7806c0b82b461a37df808a9ffd66a492b
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-17 10:37:01 +0900

    ✨ 遅延情報のCMS化を完了しました

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: nodejs({
+        mode: 'middleware' // または'standalone'
+      }),
+      output: 'hybrid',
+});
