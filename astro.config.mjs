d13cc70468a53f82dcaf271a49236adb581d1c1d
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-17 10:43:28 +0900

    ⚗️ これで治るかも？？

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: nodejs({
-        mode: 'middleware' // または'standalone'
-      }),
-      output: 'hybrid',
 });
