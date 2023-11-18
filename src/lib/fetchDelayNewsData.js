d0da5aeadc94ee1b3dd13ffc1d9e38cc006aaa47
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-18 22:53:14 +0900

    ✅ 実験、非同期処理ができていないかも

--- src/lib/fetchDelayNewsData.js
+++ src/lib/fetchDelayNewsData.js
@@ -1,23 +1,29 @@
 const delayValues = [];
-export function fetchDelayNewsData() {
+
+export async function fetchDelayNewsData() {
+  try {
     var scriptUrl = import.meta.env.PUBLIC_GOOGLE_API_KEY;
     // fetchを使用してデータを取得
     const newDelayValues = [];
-    fetch(scriptUrl)
-      .then(response => response.json())
-      .then(data => {
-  
-      for (let i = 1; i < data.length; i++) {
-        let nowTime = new Date(data[i][2]);
-        newDelayValues.push({
-          name: data[i][0],
-          updateAt: nowTime.getHours() + ":" + nowTime.getMinutes(),
-          delay: data[i][1],
-        });
-      }
-      delayValues.splice(0, delayValues.length, ...newDelayValues)
-      })
-      .catch(error => alert('Error:', error));
-      console.log(delayValues);
+
+    const response = await fetch(scriptUrl);
+    const data = await response.json();
+    for (let i = 1; i < data.length; i++) {
+      let nowTime = new Date(data[i][2]);
+      newDelayValues.push({
+        name: data[i][0],
+        updateAt: nowTime.getHours() + ":" + nowTime.getMinutes(),
+        delay: data[i][1],
+      });
+      console.log("data", data[i][0], data[i][1], data[i][2]);
+    }
+
+    delayValues.splice(0, delayValues.length, ...newDelayValues);
+    console.log(delayValues);
+
     return delayValues;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error; // エラーを再スローして呼び出し元でハンドリングできるようにする
+  }
 }
