6f9d756dd5d15250641b18e8d5e1638531fb9a0c
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-18 22:55:12 +0900

    🐛 CMSのfetchうまくいきました

--- src/lib/fetchDelayNewsData.js
+++ src/lib/fetchDelayNewsData.js
@@ -15,15 +15,13 @@ export async function fetchDelayNewsData() {
         updateAt: nowTime.getHours() + ":" + nowTime.getMinutes(),
         delay: data[i][1],
       });
-      console.log("data", data[i][0], data[i][1], data[i][2]);
     }
 
     delayValues.splice(0, delayValues.length, ...newDelayValues);
-    console.log(delayValues);
 
     return delayValues;
   } catch (error) {
     console.error('Error:', error);
-    throw error; // エラーを再スローして呼び出し元でハンドリングできるようにする
+    throw error;
   }
 }
