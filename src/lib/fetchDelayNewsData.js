f5e2afb5f453c607225730bf21cc37472dcbd73e
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-20 05:04:07 +0900

    ⚡️ コードの更新

--- src/lib/fetchDelayNewsData.js
+++ src/lib/fetchDelayNewsData.js
@@ -2,13 +2,15 @@ const delayValues = [];
 
 export async function fetchDelayNewsData() {
   try {
-    var scriptUrl = import.meta.env.PUBLIC_GOOGLE_API_KEY;
+    var scriptUrl = "https://script.google.com/macros/s/AKfycbzGDVB1KrosnxhTdX-TsP-_TW6w19PQF2UQ8v3OkPIsu5IZzweF031sKcvYoTKzzlyf/exec";
     // fetchを使用してデータを取得
     const newDelayValues = [];
 
     const response = await fetch(scriptUrl);
-    const data = await response.json();
-    for (let i = 1; i < data.length; i++) {
+    const jsonData = await response.json();
+    const data = jsonData[1];
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
       let nowTime = new Date(data[i][2]);
       newDelayValues.push({
         name: data[i][0],
@@ -20,6 +22,7 @@ export async function fetchDelayNewsData() {
     delayValues.splice(0, delayValues.length, ...newDelayValues);
 
     return delayValues;
+    return [];
   } catch (error) {
     console.error('Error:', error);
     throw error;
