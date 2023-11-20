6ce7e8062254d62f2d0236ca87e01e7cc8ceaf9a
hengin-eer <hirokix0608@gmail.com>
2023-11-20 15:35:44 +0900

    art: #4 配列長ベースのコードに変更

--- src/lib/getTornamentWinner.ts
+++ src/lib/getTornamentWinner.ts
@@ -1,7 +1,7 @@
 export function getWinner(sport: string, fetchData: Array<Array<string>>) {
     const gDom = (id: string) => document.querySelector(`.${sport} ${id}`);
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < fetchData.length; i++) {
         if ((fetchData[i][0] === fetchData[i][2]) && (fetchData[i][2] !== "0")) {
             let round = gDom(`.round${i + 1}A`).children[1];
             round.classList.add('winner');
@@ -11,7 +11,7 @@ export function getWinner(sport: string, fetchData: Array<Array<string>>) {
             round.classList.add('winner');
         }
     }
-    if (fetchData[19][2] !== "0") {
+    if (fetchData[fetchData.length - 1][2] !== "0") {
         let round = gDom(`.round_final`).children[2];
         round.classList.add('winner');
     }
